4dffcfd6e6860a43ed7cc9cca6f1a7d6e0a64f62
Erwin Greven <erwin.greven@kiwa.nl>
2016-05-09 09:39:36 +0200

    Evaluatie van de stappen vergelijkbaar met hsps, hspk en tev

--- js/HsplController.js
+++ js/HsplController.js
@@ -17,8 +17,15 @@
             iso02: { value: false, text: "Nee" }
         }
 
+        vm.capacitieveToelichting = null;
+        vm.mechanischeToelichting = null;
+        vm.weerstandsToelichting = null;
+        vm.inductieveToelichting = null;
+        vm.thermischeToelichting = null;
+        vm.step122b = false;
+
         var hsplVariables = {
-            hartafstand: 40,
+            hartafstand: null,
             leidinggeisoleerd: vm.isolatie.iso01,
             capacitievebeinvloeding: false,
             weerstandsbeinvloeding: false,
@@ -122,101 +129,29 @@
         vm.hsplVariables = hsplVariables;
         vm.evaluateStep = function () {
             if (vm.currentStep == "1") {
-                if (vm.hsplVariables.leidinggeisoleerd.value) {
-                    vm.hsplVariables.weerstandsbeinvloeding = true;
-                    vm.hsplVariables.inductievebeinvloeding = true;
-                    vm.currentStep = "1.1";
-                }
-                else {
-                    vm.hsplVariables.capacitievebeinvloeding = true;
-                    vm.currentStep = "1.2";
-                }
-                stack.push("1");
+                evaluateStep1();
                 return;
             }
             if (vm.currentStep == "1.1") {
-                if (hsplVariables.hartafstand < 50) {
-                    hsplVariables.capacitievebeinvloeding = false;
-                    vm.hsplVariables.capatoelichting = step3.I();
-                    vm.currentStep = "9";
-                }
-                else {
-                    hsplVariables.capacitievebeinvloeding = true;
-                    if (hsplVariables.hartafstand < 58.9) {
-                        hsplVariables.mechanischebeinvloeding = false;
-                        vm.currentStep = "1.1.2a";
-                    }
-                    else {
-                        hsplVariables.mechanischebeinvloeding = true;
-                        vm.currentStep = "9";
-                    }
-                }
-                stack.push("1.1");
+                evaluateStep11();
                 return;
             }
             if (vm.currentStep == "1.1.2a") {
-                if (vm.afstandTotMast < vm.selectedMastbeeld.hoogte) {
-                    vm.hsplVariables.mechanischebeinvloeding = false;
-                    vm.hsplVariables.toelichting = "Mechanische beschadiging: Treed in overleg!";
-                    vm.currentStep = "9";
-                }
-                else {
-                    vm.hsplVariables.mechanischebeinvloeding = true;
-                    vm.currentStep = "9";
-                }
-                stack.push("1.1.2a");
+                evaluateStep112a();
                 return;
             }
             if (vm.currentStep == "1.2") {
-                if (hsplVariables.hartafstand < 50) {
-                    hsplVariables.weerstandsbeinvloeding = false;
-                    vm.currentStep = "1.2.2b";
-                }
-                else {
-                    hsplVariables.weerstandsbeinvloeding = true;
-                    vm.currentStep = "1.2.2";
-                }
-                stack.push("1.2");
+                evaluateStep12();
                 return;
             }
             if (vm.currentStep == "1.2.2b") {
-                if (vm.afstandTotMast < 50) {
-                    hsplVariables.weerstandsbeinvloeding = false;
-                    //toelicht: overbruggingsspanning, aanraakspanning  en doorslag Step3 (VI)
-                    vm.hsplVariables.toelichting = step3.VI();
-                }
-                else {
-                    hsplVariables.weerstandsbeinvloeding = true;
-                }
-                vm.currentStep = "1.2.2";
-                stack.push("1.2.2b");
+                evaluateStep122b();
                 return;
             }
 
             // Bepaal Petersburg
             if (vm.currentStep == "1.2.2") {
-                if (functionLibrary.PetersBurg(vm.selectedMastbeeld, vm.parallelloop, vm.afstandTotMast)) {
-                    hsplVariables.inductievebeinvloeding = true;
-                }
-                else {
-                    hsplVariables.inductievebeinvloeding = false;
-                    hsplVariables.toelichting += step3.VII();
-                }
-                if (hsplVariables.hartafstand < 58.9) {
-                    hsplVariables.mechanischebeinvloeding = false;
-                    if (vm.afstandTotMast < vm.selectedMastbeeld.hoogte) {
-                        vm.hsplVariables.mechanischebeinvloeding = false;
-                        vm.hsplVariables.toelichting += "Mechanische beschadiging: Treed in overleg!";
-                    }
-                    else {
-                        vm.hsplVariables.mechanischebeinvloeding = true;
-                    }
-                }
-                else {
-                    hsplVariables.mechanischebeinvloeding = true;
-                }
-                vm.currentStep = "9";
-                stack.push("1.2.2");
+                evaluateStep122();
                 return;
             }
         };
@@ -236,6 +171,113 @@
                     return vm.masten;
                 })
         }
+
+        function evaluateStep1() {
+            if (vm.hsplVariables.leidinggeisoleerd.value) {
+                vm.hsplVariables.weerstandsbeinvloeding = true;
+                vm.hsplVariables.inductievebeinvloeding = true;
+                vm.hsplVariables.capacitievebeinvloeding = false;
+                vm.currentStep = "1.1";
+            }
+            else {
+                vm.hsplVariables.weerstandsbeinvloeding = false;
+                vm.hsplVariables.inductievebeinvloeding = false;
+                vm.hsplVariables.capacitievebeinvloeding = true;
+                vm.currentStep = "1.2";
+            }
+            stack.push("1");
+        }
+
+        function evaluateStep11() {
+            if (hsplVariables.hartafstand < 50) {
+                hsplVariables.capacitievebeinvloeding = false;
+                vm.capacitieveToelichting = step3.I();
+                vm.currentStep = "9";
+            }
+            else {
+                hsplVariables.capacitievebeinvloeding = true;
+                vm.capacitieveToelichting = null;
+                vm.mechanischeToelichting = null;
+                if (hsplVariables.hartafstand < 58.9) {
+                    hsplVariables.mechanischebeinvloeding = false;
+                    vm.currentStep = "1.1.2a";
+                }
+                else {
+                    hsplVariables.mechanischebeinvloeding = true;
+                    vm.currentStep = "9";
+                }
+            }
+            stack.push("1.1");
+        }
+
+        function evaluateStep112a() {
+            if (vm.afstandTotMast < vm.selectedMastbeeld.hoogte) {
+                vm.hsplVariables.mechanischebeinvloeding = false;
+                vm.mechanischeToelichting = "Mechanische beschadiging: Treed in overleg!";
+            }
+            else {
+                vm.hsplVariables.mechanischebeinvloeding = true;
+                vm.mechanischeToelichting = null;
+            }
+            vm.currentStep = "9";
+            stack.push("1.1.2a");
+        }
+
+        function evaluateStep12() {
+            if (hsplVariables.hartafstand < 50) {
+                hsplVariables.weerstandsbeinvloeding = false;
+                vm.currentStep = "1.2.2b";
+                vm.step122b = true;
+            }
+            else {
+                hsplVariables.weerstandsbeinvloeding = true;
+                vm.step122b = false;
+                vm.currentStep = "1.2.2";
+            }
+            stack.push("1.2");
+        }
+
+        function evaluateStep122b() {
+            if (vm.afstandTotMast < 50) {
+                hsplVariables.weerstandsbeinvloeding = false;
+                vm.weerstandsToelichting = step3.VI();
+            }
+            else {
+                hsplVariables.weerstandsbeinvloeding = true;
+                vm.weerstandsToelichting = null;
+            }
+            vm.currentStep = "1.2.2";
+            stack.push("1.2.2b");
+        }
+
+        function evaluateStep122() {
+            if (functionLibrary.PetersBurg(vm.selectedMastbeeld, vm.parallelloop, vm.afstandTotMast)) {
+                hsplVariables.inductievebeinvloeding = true;
+                vm.inductieveToelichting = null;
+            }
+            else {
+                hsplVariables.inductievebeinvloeding = false;
+                vm.inductieveToelichting = step3.VII();
+            }
+            if (hsplVariables.hartafstand < 58.9) {
+                hsplVariables.mechanischebeinvloeding = false;
+                vm.mechanischeToelichting = null;
+                if (vm.afstandTotMast < vm.selectedMastbeeld.hoogte) {
+                    vm.hsplVariables.mechanischebeinvloeding = false;
+                    vm.mechanischeToelichting = "Mechanische beschadiging: Treed in overleg!";
+                }
+                else {
+                    vm.hsplVariables.mechanischebeinvloeding = true;
+                }
+            }
+            else {
+                hsplVariables.mechanischebeinvloeding = true;
+                vm.mechanischeToelichting = null;
+            }
+            vm.currentStep = "9";
+            stack.push("1.2.2");
+        }
+
 
     };
 } ());
